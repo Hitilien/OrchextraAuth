7bff0ff99f609402a064f623bd08bb5d4b9b5d32
Javier Garcia <jyavier@gmail.com>
2017-07-27 12:13:04 -0500

    Cambios faltantes

---
+++ Auth.framework/Headers/Auth.h
@@ -0,0 +1,19 @@
+//
+//  Auth.h
+//  Auth
+//
+//  Created by macpro.javier.garcia on 7/21/17.
+//  Copyright © 2017 macpro.javier.garcia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Auth.
+FOUNDATION_EXPORT double AuthVersionNumber;
+
+//! Project version string for Auth.
+FOUNDATION_EXPORT const unsigned char AuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Auth/PublicHeader.h>
+
+
